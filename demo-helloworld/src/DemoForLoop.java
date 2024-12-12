e7e0c1f46559669fcdfe74856f9c3307b7fd114c
SharonC10 <sharonchan1197@gmail.com>
2024-12-12 15:00:55 +0800

    241212

--- demo-helloworld/src/DemoForLoop.java
+++ demo-helloworld/src/DemoForLoop.java
@@ -6,52 +6,68 @@ public class DemoForLoop {
     x = x * 2;
     x *= 2;
     System.out.println(x); //8 ------Q1
+    //problem: can type 100 times.
+
 
     
-
-
-
-
-
-    //problem: can type 100 times.
     //For Loop
-    //for (int i = 0; i < 3; i++) { // 0,1,2 or 3 - 0
-      // <= , don't use it 
-      //System.out.println("hello");
-
-
-
-
+    //e.g 
+    // for(int X = 0 ; X < or > ? ; X ++){
+    // System.out.println(XXX); 
+    //}
 
     //! i < 3 (continue criteria)
     // i++ (modifier)
     // ! you have to let the modifier work with "continue criteria", so that 
-    // ! the contunue crtieria at the end become false 
+    // ! the continue crtieria at the end become false 
     //! for (initilization; condition ; modifier)
 
+
+    for (int i = 0; i < 3; i++) { // 0,1,2 or 3 - 0
+      //<= , don't use it 
+      System.out.println("hello");
     //Step1. int i = 0;  go 1 times
     
     //Step2. i <3 (question) -> true
     //checking if i < 3
 
-    //Step3. if true, print hello
+    //Step3. if true, print hello-------------0
     //enter into code block
 
     //Step4. i++, i become 1
     // i++ -> i + 1 --> 0 + 1 for next
 
 
-    //Step5: i < 3 (queston ) ->true
+    //Step5: i (1)now < 3 (queston ) ->true
     // checking with i < 3?? 
-    // now i become 1 -> true
 
-    //Step6. if true , print hello 
-    //Shtpe7. i++ , i become 2
-    //Step8. i M 3 (question ) -> true 
-    //Step9. if true , print hello
+    //Step6. if true , print hello ---------------1
+
+    //Step7. i++ , i become 2
+    // i++ -> (1) + (1) -> 2
+
+    //Step8. i (2)now < 3 (question ) -> true 
+
+    //Step9. if true , print hello-----------------2
+
     //Step10. i++ , i become 3
+
     //Step11. i <3 (question ) -> false 
-    //Step12. exit for loop
+
+    // !!! Step12. exit for loop !!!
+    }
+
+    //Test
+    for (int f = 0 ; f < 5; f++){ //0,1,2,3,4 ; 5-0 -> 5times
+    System.out.println("Sharon");
+    }
+
+
+    for (int yy = 0 ; yy < 10 ; yy ++){
+      System.out.println(yy);
+    }
+    // yy = 0 like identify ; yy < 10 -> like how many time to repeat; 
+    //yy ++ -> the calculate ( yy + 1)
 
 
 
@@ -60,11 +76,7 @@ public class DemoForLoop {
 
 
 
-    //for (int i = 0 ; i < 5; i++){ //0,1,2,3,4 ; 5-0 -> 5times
-      //System.out.println(i);
-    //}
-
-    //print event numbers
+    //print even numbers
     for (int i = 0 ; i < 10; i++){
       if (i % 2 == 0) {
         System.out.println(i);
@@ -77,6 +89,18 @@ public class DemoForLoop {
         System.out.println(a);// 12, 24,36, 48, 60 ,72,84, 96
       }
     }
+
+    // odd number
+    for ( z = 0 ; z < 10 ; z++){
+      if (z % 2 == 1){
+        System.out.println(z);
+      }
+    }
+
+
+
+
+
 
 
 
@@ -119,6 +143,20 @@ public class DemoForLoop {
 
     }
     
+      //1. check if 'd' exists in the string.
+      // for loop + charAt
+
+      String str = "abcdefijk";
+     
+      for ( int r = 0 ; r <= 10  ; r++ ){ // loop what? 
+        // loop str -> having 'd'?
+        str = str + r; { char isCharExist = str.charAt(r);
+      }
+      
+      System.out.println(isCharExist);
+
+
+      }
 
 
   }
